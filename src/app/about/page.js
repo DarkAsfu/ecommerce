c53d8bfc4b062a019e45aaf8d503127f68630fba
ashraful-me <heyashrafulislam.me@gmail.com>
2025-07-23 04:14:01 +0600

    In about page delivery info section complete

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,11 @@
+import DeliveryInfo from "@/components/about/DeliveryInfo";
 import SectionBanner from "@/components/reusable/SectionBanner";
 
 export default function AboutPage() {
   return (
     <div>
       <SectionBanner title="About Us" />
+      <DeliveryInfo/>
     </div>
   );
 }
