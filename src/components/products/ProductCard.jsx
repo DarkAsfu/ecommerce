b54ad099fedfd253a7022ee0473507ffd531a5ce
DarkAsfu <ashraful.islam.asfu@gmail.com>
2025-07-29 06:16:41 +0600

    Api branch

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = ({ product, isWishlist }) => {
     >
       <div className="relative border px-[57px] md:h-[450px] py-[52px]">
         <img
-          src={product.image.src}
+          src={product.product_image}
           alt=""
           className="max-h-max object-contain mx-auto"
         />
@@ -26,15 +26,15 @@ const ProductCard = ({ product, isWishlist }) => {
 
       <div className="relative bg-white pt-3">
         <h3 className="text-[21.342px] font-lato leading-[32.01px] text-heading group-hover:underline group-hover:underline-offset-4">
-          {product.name}
+          {product.product_name}
         </h3>
 
         <div className="flex items-center gap-x-[5.34px]">
           <p className="text-[21.342px] font-lato leading-[32.01px] text-heading">
-            ${product.salePrice}
+            ${product.discount_price}
           </p>
           <p className="text-[21.342px] font-lato leading-[32.01px] text-secondary line-through">
-            ${product.regularPrice}
+            ${product.regular_price}
           </p>
         </div>
       </div>
@@ -43,50 +43,3 @@ const ProductCard = ({ product, isWishlist }) => {
 };
 
 export default ProductCard;
-
-
-// import { Heart } from "lucide-react";
-
-// const ProductCard = ({ product, isWishlist }) => {
-//   return (
-//     <a
-//       key={product.id}
-//       href="#"
-//       className="group relative block overflow-hidden"
-//     >
-//       <div className="relative border px-[57px] md:h-[450px] py-[52px]">
-//         <img
-//           src={product.product_image}
-//           alt=""
-//           className="max-h-max object-contain mx-auto"
-//         />
-
-//         {/* Heart Icon */}
-//         {isWishlist ? <div className="absolute top-4 right-4 p-2 rounded-full bg-[#F5F5F5] shadow-md border border-primary opacity-100  transition-opacity duration-300 z-10">
-//             <Heart className="h-5 w-5 text-red-500 fill-red-500 " />
-//           </div> : (
-//           <div className="absolute top-4 right-4 p-2 rounded-full bg-white shadow-md border border-primary opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-//             <Heart className="h-5 w-5 text-primary " />
-//           </div>
-//         )}
-//       </div>
-
-//       <div className="relative bg-white pt-3">
-//         <h3 className="text-[21.342px] font-lato leading-[32.01px] text-heading group-hover:underline group-hover:underline-offset-4">
-//           {product.product_name}
-//         </h3>
-
-//         <div className="flex items-center gap-x-[5.34px]">
-//           <p className="text-[21.342px] font-lato leading-[32.01px] text-heading">
-//             ${product.discount_price}
-//           </p>
-//           <p className="text-[21.342px] font-lato leading-[32.01px] text-secondary line-through">
-//             ${product.regular_price}
-//           </p>
-//         </div>
-//       </div>
-//     </a>
-//   );
-// };
-
-// export default ProductCard;
